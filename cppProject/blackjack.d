460e334ccee98e4886e944ea597dc032ec258a93
CDenille <carringtondenille@gmail.com>
2023-07-25 13:56:39 -0400

    change complier in makeFile

--- cppProject/blackjack.d
+++ cppProject/blackjack.d
@@ -1,2 +1,2 @@
 obj/blackjack.o: src/blackjack.cpp src/blackjack.h src/card.h src/deck.h \
-  src/player.h src/dealer.h
+ src/player.h src/dealer.h
--- cppProject/main.d
+++ cppProject/main.d
@@ -1,2 +1,2 @@
 obj/main.o: src/main.cpp src/blackjack.h src/card.h src/deck.h \
-  src/player.h src/dealer.h
+ src/player.h src/dealer.h
